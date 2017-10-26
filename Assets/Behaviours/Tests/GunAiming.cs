a57e0fae343283ae8a79bca776ad18b6b586da4a
Jdasi <silvacore@hotmail.co.uk>
2017-10-26 14:57:54 +0100

    added shoot hits and ejected casings

--- Assets/Behaviours/Tests/GunAiming.cs
+++ Assets/Behaviours/Tests/GunAiming.cs
@@ -4,11 +4,20 @@ using UnityEngine;
 
 public class GunAiming : MonoBehaviour
 {
+    [Header("Parameters")]
+    [SerializeField] float aim_force = 10;
+    [SerializeField] float recoil_force;
+    [SerializeField] float ejection_force;
+    [SerializeField] float hit_force;
+    [SerializeField] LayerMask hit_layers;
+
+    [Header("References")]
     [SerializeField] Rigidbody2D gun_aimer;
     [SerializeField] Rigidbody2D gun;
-    [SerializeField] private GameObject particle_effect;
-    [SerializeField] private Transform muzzle;
-    [SerializeField] float aim_force = 10;
+    [SerializeField] Transform ejection_point;
+    [SerializeField] GameObject particle_effect;
+    [SerializeField] Transform muzzle;
+    [SerializeField] GameObject bullet_casing_prefab;
 
     private Vector2 target_pos = new Vector2();
     private bool fire = false;
@@ -18,14 +27,38 @@ public class GunAiming : MonoBehaviour
     {
         UpdateTargetPos();
         fire = Input.GetButtonDown("Fire1");
+
         if (fire)
         {
-            gun.AddForce(gun.transform.up * 10000);
+            // Recoil.
+            gun.AddForce(gun.transform.up * recoil_force);
+
             var particle = Instantiate(particle_effect);
             particle.transform.position = muzzle.position;
             particle.transform.rotation = muzzle.rotation;
+
+            AudioManager.PlayOneShot("gun_shot");
+
+            var casing_clone = Instantiate(bullet_casing_prefab, ejection_point.position, gun.transform.rotation);
+            casing_clone.GetComponent<Rigidbody2D>().AddForce(gun.transform.right * ejection_force);
+
             Destroy(particle, 5);
+            HitScan();
         }
+    }
+
+
+    void HitScan()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(muzzle.position, -gun.transform.up, Mathf.Infinity, hit_layers);
+        Debug.DrawLine(muzzle.position, muzzle.position + (-gun.transform.up * 1000), Color.green, 3);
+
+        if (hit.rigidbody == null)
+            return;
+
+        Debug.Log(hit.rigidbody.name);
+        Vector3 dir = (hit.transform.position - gun.transform.position).normalized;
+        hit.rigidbody.AddForce(dir * hit_force);
     }
 
 
---
+++ Assets/Classes/TempSceneRefs.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct TempSceneRefs
+{
+    /*
+    public StatTracker stat_tracker
+    {
+        get
+        {
+            if (stat_tracker_ == null)
+                stat_tracker_ = GameObject.FindObjectOfType<StatTracker>();
+
+            return stat_tracker_;
+        }
+    }
+
+
+    private StatTracker stat_tracter_;
+    */
+}
