bf02559085d09e26453959ddb0616090b29f02e1
zbigniew2.kajota <zbigniew2.kajota@live.uwe.ac.uk>
2017-10-27 19:39:57 +0100

    Added basic map functionality.

---
+++ Assets/MapScripts/MissionButton.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MissionButton : MonoBehaviour {
+
+    public List<GameObject> briefs;
+
+    // Use this for initialization
+    void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    public void StartMission(int missionID)
+    {
+        
+    }
+
+    public void BriefMission(GameObject Mission)
+    {
+        foreach (GameObject brief in briefs)
+        {
+            brief.SetActive(false);
+        }
+        Mission.SetActive(true);
+    }
+}
