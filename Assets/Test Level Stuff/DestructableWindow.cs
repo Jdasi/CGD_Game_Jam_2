011c93c1936f379b76f16a7403a35927711e22fc
Samuel Harden <samuel2.harden@live.uwe.ac.uk>
2017-10-28 02:44:09 +0000

    Destructable Glass Basics
    Check Level 3, also try grappling through panes, while using slow time :
    P

---
+++ Assets/Test Level Stuff/DestructableWindow.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestructableWindow : MonoBehaviour
+{
+    [SerializeField] GameObject broken_window;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            Instantiate(broken_window, transform.position, broken_window.transform.rotation);
+
+            ; Destroy(this.gameObject);
+        }
+    }
+}
