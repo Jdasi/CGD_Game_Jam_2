3e4ad7510cbd5c332536bd1ac1c3d7cbe61998d0
Samuel Harden <samuel2.harden@live.uwe.ac.uk>
2017-10-28 21:45:19 +0000

    Glass now Shatters from impact
    Glass should 'shatter' based on point hit

--- Assets/Behaviours/Utility/DestructableWindow.cs
+++ Assets/Behaviours/Utility/DestructableWindow.cs
@@ -5,12 +5,25 @@ using UnityEngine;
 public class DestructableWindow : MonoBehaviour
 {
     [SerializeField] GameObject broken_window;
+    [SerializeField] GameObject force_explosion;
     [SerializeField] AudioClip break_clip;
 
 
     public void Break()
     {
         Instantiate(broken_window, transform.position, broken_window.transform.rotation);
+        AudioManager.PlayOneShot(break_clip);
+
+        Destroy(this.gameObject);
+    }
+
+
+    public void BreakShot(Vector3 _explosion_point)
+    {
+        var br_window = Instantiate(broken_window, transform.position, broken_window.transform.rotation);
+        var explosion = Instantiate(force_explosion, _explosion_point, force_explosion.transform.rotation);
+        //explosion.transform.SetParent(br_window.transform);
+
         AudioManager.PlayOneShot(break_clip);
 
         Destroy(this.gameObject);
