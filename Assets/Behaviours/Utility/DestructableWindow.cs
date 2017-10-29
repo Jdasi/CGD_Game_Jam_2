379a800955a1e825215782e404af747040184646
Samuel Harden <samuel2.harden@live.uwe.ac.uk>
2017-10-29 12:39:05 +0000

    Added Flipped Window Prefab
    more random glass pieces when glass shatters, just use the secondary
    prebab

--- Assets/Behaviours/Utility/DestructableWindow.cs
+++ Assets/Behaviours/Utility/DestructableWindow.cs
@@ -8,9 +8,13 @@ public class DestructableWindow : MonoBehaviour
     [SerializeField] GameObject force_explosion;
     [SerializeField] AudioClip break_clip;
 
+    private bool fired;
+
 
     public void Break()
     {
+        fired = true;
+
         Instantiate(broken_window, transform.position, broken_window.transform.rotation);
         AudioManager.PlayOneShot(break_clip);
 
@@ -20,9 +24,8 @@ public class DestructableWindow : MonoBehaviour
 
     public void BreakShot(Vector3 _explosion_point)
     {
-        var br_window = Instantiate(broken_window, transform.position, broken_window.transform.rotation);
-        var explosion = Instantiate(force_explosion, _explosion_point, force_explosion.transform.rotation);
-        //explosion.transform.SetParent(br_window.transform);
+        Instantiate(broken_window, transform.position, broken_window.transform.rotation);
+        Instantiate(force_explosion, _explosion_point, force_explosion.transform.rotation);
 
         AudioManager.PlayOneShot(break_clip);
 
@@ -34,6 +37,7 @@ public class DestructableWindow : MonoBehaviour
     {
         if (other.gameObject.tag == "Player")
         {
+            if(!fired)
             Break();
         }
     }
