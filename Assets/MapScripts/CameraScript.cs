17ca8d982f4c94a734bcd10e5250c0bb66d11bc6
zbigniew2.kajota <zbigniew2.kajota@live.uwe.ac.uk>
2017-10-29 14:46:02 +0000

    Improved map functionality

---
+++ Assets/MapScripts/CameraScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour {
+
+    private Animator cameraZoom;
+    public Camera cam;
+    // Use this for initialization
+    void Start ()
+    {
+        cameraZoom = cam.gameObject.GetComponent<Animator>();
+        cameraZoom.SetTrigger("StartZoom");
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+	}
+
+    public void MissionOne()
+    {
+        cameraZoom.SetTrigger("Mission1Enter");
+    }
+    public void MissionTwo()
+    {
+        cameraZoom.SetTrigger("Mission1Enter");
+    }
+    public void MissionThree()
+    {
+        cameraZoom.SetTrigger("Mission1Enter");
+    }
+    public void ReturnToMap()
+    {
+        cameraZoom.SetTrigger("ReturnToMap");
+        cameraZoom.ResetTrigger("Mission1Enter");
+    }
+}
