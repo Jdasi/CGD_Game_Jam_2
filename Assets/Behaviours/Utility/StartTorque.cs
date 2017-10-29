745307e7bbbd2200fc198b6e0398a22c5d4aadd2
Tom Turner <tom.turner2@googlemail.com>
2017-10-29 13:30:12 +0000

    Start Screen

---
+++ Assets/Behaviours/Utility/StartTorque.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class StartTorque : MonoBehaviour
+{
+    [SerializeField] private float torque;
+
+
+	void Start ()
+    {
+		GetComponent<Rigidbody2D>().AddTorque(torque);
+	}
+
+}
