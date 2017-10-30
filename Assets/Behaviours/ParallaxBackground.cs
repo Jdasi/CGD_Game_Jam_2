dd23927e9db65d5601a7193b4ef6ff6c70c9eeb5
Jdasi <silvacore@hotmail.co.uk>
2017-10-30 12:48:33 +0000

    crapola parallax script

---
+++ Assets/Behaviours/ParallaxBackground.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxBackground : MonoBehaviour
+{
+    [SerializeField] float smoothing = 1;
+
+    private Transform rel;
+    private Vector3 prev_pos;
+    private float parallax_scale;
+
+
+    void Start()
+    {
+        rel = Camera.main.transform;
+        prev_pos = rel.position;
+
+        parallax_scale = transform.position.z * -1;
+    }
+
+
+    void Update()
+    {
+        float parallax = (prev_pos.x - rel.transform.position.x) * parallax_scale;
+        float target_x = transform.position.x + parallax;
+
+        Vector3 target_pos = new Vector3(target_x, transform.position.y, transform.position.z);
+        transform.position = Vector3.Lerp(transform.position, target_pos, smoothing * Time.deltaTime);
+
+        prev_pos = rel.position;
+    }
+
+}
