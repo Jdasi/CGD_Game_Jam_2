9c123977f63aa38dac57bc46e826f6869edcb614
Tom Turner <tom.turner2@googlemail.com>
2017-10-31 15:07:59 +0000

    level end

--- Assets/Behaviours/Managers/LevelManager.cs
+++ Assets/Behaviours/Managers/LevelManager.cs
@@ -1,20 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour
 {
-    [SerializeField] Vector3 sniper_cam_goto;
+    [SerializeField] UnityEvent on_level_complete;
     [SerializeField] float level_time_limit;
     [SerializeField] float final_killcam_delay = 3.0f;
 
     public float level_timer;
-    private bool target_dead;
+    public bool level_finished { get; set; }
 
 
 
     void Start()
     {
+        level_finished = false;
         // Set Timer
         level_timer = 0;
     }
@@ -24,8 +27,11 @@ public class LevelManager : MonoBehaviour
     {
         CheckTimer();
 
-        if (target_dead)
-            StartCoroutine(EndLevel(final_killcam_delay));
+        if (!level_finished)
+            return;
+
+        if (Input.anyKey)
+            SceneManager.LoadScene(1);
     }
 
 
@@ -44,38 +50,18 @@ public class LevelManager : MonoBehaviour
     }
 
 
-    // These get called by Triggers in the Level
-    public void SetCameraPlayer()
-    {
-        GameManager.scene.camera_manager.SetTarget(GameManager.scene.player.bod.transform);
-    }
-
-
-    public void SetCameraSniper()
-    {
-        GameManager.scene.camera_manager.SetTarget(sniper_cam_goto);
-    }
-
-
-    void OnDrawGizmos()
-    {
-        Gizmos.color = Color.red;
-        Gizmos.DrawSphere(sniper_cam_goto, 1);
-    }
-
-
     // Called By the Target
     public void TargetKilled()
     {
-        target_dead = true;
+        level_finished = true;
+        StartCoroutine(EndLevel(final_killcam_delay));
     }
 
     
     IEnumerator EndLevel(float _delay)
     {
         yield return new WaitForSeconds(_delay);
-
-        Debug.Log("Target Dead, End Level");
+        on_level_complete.Invoke();
     }
 }
 
