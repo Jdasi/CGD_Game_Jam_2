46a9657009c7fb7cd627e7dc25b4ab6ec771a4e1
Samuel Harden <samuel2.harden@live.uwe.ac.uk>
2017-10-31 11:59:52 +0000

    Turrets now have explosion

---
+++ Assets/Behaviours/Utility/SpawnExplosion.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnExplosion : MonoBehaviour
+{
+    [SerializeField] GameObject explosion;
+
+    public void Explosion()
+    {
+        Instantiate(explosion, transform.position, transform.rotation);
+    }
+}
